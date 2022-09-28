78c07988930a77d8e13ee0b705285bfa13ac1855
Paulo Guilherme <paulo.guilherme.arruda1@gmail.com>
2022-09-28 20:13:04 -0300

    fix getMatchByPuuid router

--- src/routes.ts
+++ src/routes.ts
@@ -15,7 +15,7 @@ routes.post('/api/sumonnerApi',  new SumonnerController().getSumonner)
 
 routes.post('/infoMatchCreate', new InfoMatchController().create)
 // routes.post('/infoMatchList', new InfoMatchController().List)
-routes.post('/getMatchBypuuid', new GetMatcheByPuuidController().List)
+routes.post('/api/getMatchBypuuid', new GetMatcheByPuuidController().List)
 
 
 export default routes
